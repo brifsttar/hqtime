f1844ab8908f7eadb981f12b1515175f12d3d030
Bertrand Richard <bertrand.richard@ifsttar.fr>
2025-11-02 11:53:35 +0100

    First draft for Tempo, obviously untested

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# HQTime Auto clock-in
+# Tempo Auto Badgeage
 
-My workplace forces me to clock-in even when working from home, using a terrible web interface. So I made this script that clocks in for me.
+My workplace forces me to clock-in four times a day, every day. So I made this script that does it for me.
+
+## Usage
+
+### Requirements & Installation
+
+- Python 3.8+ (probably works with older versions too, but untested)
+- A system keyring supported by the `keyring` Python package (e.g. `secret-service` on Linux, `Keychain` on macOS, `Credential Locker` on Windows)
+- Install dependencies:
+- ```bash
+  pip install -r requirements.txt
+  ```
+- Install `playwright` browsers:
+- ```bash
+  playwright install
+  ```
+
+### Credentials
+
+You first need to store your credentials in the system keyring:
+
+```bash
+python -m keyring set tempo prenom.nom
+```
+
+### Running the script
+
+```
+usage: main.py [-h] [--badgeage-times BADGEAGE_TIMES BADGEAGE_TIMES BADGEAGE_TIMES BADGEAGE_TIMES] [--ttf_day TTF_DAY] [--random-offset-range RANDOM_OFFSET_RANGE] username
+
+positional arguments:
+  username              Username for tempo.univ-eiffel.fr, as stored on the local keyring
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --badgeage-times BADGEAGE_TIMES BADGEAGE_TIMES BADGEAGE_TIMES BADGEAGE_TIMES
+                        Times around which to badge (default: [8, 12, 12.75, 18])
+  --ttf_day TTF_DAY     Day of the week for "télétravail flottant" (0=Monday, 6=Sunday) (default: 2)
+  --random-offset-range RANDOM_OFFSET_RANGE
+                        Range in minutes for random offset around badgeage times (default: 30)
+```
+
+### Scheduling
+
+You can use `cron` (Linux/macOS) or Task Scheduler (Windows) to run the script at system startup or at specific times.
---
+++ main.py
@@ -0,0 +1,158 @@
+import argparse
+from datetime import datetime as dt
+from random import randrange
+import logging as log
+import time
+import re
+
+from playwright.sync_api import sync_playwright
+import keyring
+
+def get_badgeage_times(in_badgeage_times, random_offset_range):
+    now = dt.now()
+    for h in in_badgeage_times:
+        # Offset in minutes
+        offset = randrange(random_offset_range) - (random_offset_range / 2.)
+        # To hour
+        h += offset / 60
+        yield now.replace(hour=int(h), minute=int(60 * (h % 1)), second=0)
+
+def main():
+    log.basicConfig(
+        format='%(asctime)-15s [%(levelname)-8s]: %(message)s',
+        level=log.INFO,
+        handlers=[
+            log.FileHandler("tempo.log"),
+            log.StreamHandler()
+        ]
+    )
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        'username',
+        type=str,
+        help='Username for tempo.univ-eiffel.fr, as stored on the local keyring'
+    )
+    parser.add_argument(
+        '--badgeage-times',
+        nargs=4,
+        type=float,
+        default=[8, 12, 12.75, 18],
+        help='Times around which to badge'
+    )
+    parser.add_argument(
+        '--ttf_day',
+        type=int,
+        default=2,
+        help='Day of the week for "télétravail flottant" (0=Monday, 6=Sunday)'
+    )
+    parser.add_argument(
+        '--random-offset-range',
+        type=int,
+        default=30,
+        help='Range in minutes for random offset around badgeage times'
+    )
+    args = parser.parse_args()
+    badgeage_times = list(get_badgeage_times(args.badgeage_times, args.random_offset_range))
+    now = dt.now()
+    clock_out = badgeage_times[-1]
+    if now > clock_out:
+        log.info(f"{now} is after end of day")
+        return
+    if now.weekday() > 4:
+        log.info(f"{now} is not a working day")
+        return
+    while True:
+        try:
+            with sync_playwright() as p:
+                browser = p.chromium.launch(headless=True)
+                ctx = browser.new_context()
+                page = ctx.new_page()
+
+                # 1. Login on CAS
+                page.goto("http://tempo.univ-eiffel.fr/")
+                page.fill("#username", args.username)
+                pwd = keyring.get_password("tempo", args.username)
+                page.fill("#password", pwd)
+                del pwd
+                page.click('button[type="submit"]')
+                page.wait_for_url("**/webquartz/ux/home")
+
+                # 2. Check if the day has a legitimate "absence"
+                page.get_by_role("button", name="Menu").click()
+                page.get_by_role("menuitem").click()
+                page.locator("#menu-item-3").click()
+
+                # 2.1 Select the current week
+                period = page.locator("[class*='period-planning-picker']")
+                period.locator("[class*='variant-action']").click()
+                calendar = page.locator("[class*='period-planning-action-buttons']")
+                calendar.locator("[class*='variant-action']").click()
+
+                # 2.2 Find current day index
+                header = page.locator("[class*='planning-header']")
+                current_day = header.locator("[class*='current']")
+                parent =  current_day.locator("..").element_handle()
+                current_handle = current_day.element_handle()
+                today = parent.evaluate("(parent, el) => Array.from(parent.children).indexOf(el)", current_handle)
+
+                # 2.3 Check for absences on current day
+                planning = page.locator("[class*='planning-individual']")
+                absences = planning.locator("[class*='planning-item__abs']")
+                for i in range(absences.count()):
+                    element = absences.nth(i)
+                    style = element.get_attribute("style")
+                    match = re.search(r"grid-column:\s*(\d+)\s*/", style)
+                    if match:
+                        absence_day = (int(match.group(1)) - 1) // 2
+                        if absence_day == today:
+                            log.info("Today has a legitimate absence, exiting")
+                            browser.close()
+                            return
+
+                # 3. Navigate to get current badgeage status
+                page.goto("http://tempo.univ-eiffel.fr/")
+                page.get_by_role("button", name="Menu").click()
+                page.get_by_role("menuitem").click()
+                page.locator("#menu-item-6").click()
+                page.locator(".clock-correction").wait_for(state="visible")
+                grid = page.get_by_role("grid")
+                rows = grid.get_by_role("row").count()
+
+                # 4. Determine next badgeage time
+                badgeages_count = rows - 1 if rows > 1 else 0
+                if badgeages_count >= len(badgeage_times):
+                    log.info("Done for the day")
+                    browser.close()
+                    return
+                next_badgeage = badgeage_times[badgeages_count]
+                now = dt.now()
+                time_to = (next_badgeage - now).total_seconds()
+
+                # 5. Badge or sleep
+                if time_to < 0:
+                    # Badgeage time!
+                    page.goto("http://tempo.univ-eiffel.fr/")
+                    page.get_by_role("button", name="Menu").click()
+                    page.get_by_role("menuitem").click()
+                    page.locator("#menu-item-5").click()
+                    primary = page.locator("button[class*='button'][class*='primary']")
+                    secondary = page.locator("button[class*='button'][class*='secondary']")
+                    if now.weekday() == args.ttf_day:
+                        log.info("Badging with télétravail flottant")
+                        secondary.click()
+                    else:
+                        log.info("Badging normally")
+                        primary.click()
+                    page.wait_for_timeout(1000)
+                    browser.close()
+                else:
+                    # Sleep until next badgeage
+                    browser.close()
+                    log.info(f"Sleeping {time_to}s until next badgeage at {next_badgeage}")
+                    time.sleep(time_to)
+        except (Exception,) as e:
+            log.exception("An error occurred, retrying...")
+
+
+if __name__ == '__main__':
+    main()
